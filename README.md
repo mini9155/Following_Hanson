7c9df34e9ae270b507537fbdce63a780b3bd71df
mini9155 <hanbv@naver.com>
2025-01-14 15:49:09 +0900

    .

--- README.md
+++ README.md
@@ -1,6 +1,18 @@
 # Follwing Hans_on
 learning machine_learing with hanson
 
+#### 가상환경 활성화가 안될 시
+
+ '''
+ Get-ExecutionPolicy
+
+ 만약 출력이 : Restricted 이면 스크립트 실행 허용 X
+
+ Set-ExecutionPolicy RemoteSigned (로컬 스크립트는 실행 가능)
+
+ 
+ ''' 
+
 #### 계층적 샘플링 - stratified smapling
 - 특정 비율에 맞춘 샘플링, 만약 여론조사시 인구의 40퍼가 여성이고, 인구의 60퍼가 남성이라면 조사시 이 비율을 맞추는 것을 의미한다
 
--- main.py
+++ main.py
@@ -16,7 +16,7 @@ def fetch_housing_data(housing_url = HOUSING_URL, housing_path=HOSUING_PATH):
     housing_tgz.extractall(path=housing_path)
     housing_tgz.close()
 
-
+#가상환경 active 실행이 안될 시
 
 
 import pandas as pd
@@ -116,15 +116,87 @@ housing = strat_train_set.copy()
 # colorbar : 옆에 색상바를 표시
 # sharex : 플롯을 여러 서브플롯으로 나눌 떄 X축을 공유할지의 대한 값
 # 아직 완벽 이해는 안감...
-housing.plot(kind="scatter", x="longitude", y="latitude", alpha=0.4,
-             s=housing["population"]/100, label="population", figsize=(10,7),
-             c="median_house_value",cmap=plt.get_cmap("jet"), colorbar=True,
-             sharex=False)
+# housing.plot(kind="scatter", x="longitude", y="latitude", alpha=0.4,
+#              s=housing["population"]/100, label="population", figsize=(10,7),
+#              c="median_house_value",cmap=plt.get_cmap("jet"), colorbar=True,
+#              sharex=False)
 
 #데이터 셋이 크지않아 corr() 이라는 함수를 사용하여 모든 특성 간의 표준 상관계수을 계산할 수 있다
 # 중간에 문자열이 있어 오류가 발생하였고 자동적으로 숫자열의 상관관계를 분석하는 numeric_only=True 를 사용
 corr_matrix = housing.corr(numeric_only=True)
 
-print(corr_matrix["median_house_value"].sort_values(ascending=False))
+# print(corr_matrix["median_house_value"].sort_values(ascending=False))
 
 # 94p까지 진행 완료
+# 
+from pandas.plotting import scatter_matrix
+
+# atrtibute = ["median_house_value", "median_income", "total_rooms",
+#              "housing_median_age"]
+
+# scatter_matrix(housing[atrtibute], figsize=(12,8))
+
+# housing.plot(kind="scatter", x="median_income", y="median_house_value",
+#              alpha=0.1)
+
+housing["room_per_househlod"] = housing["total_rooms"]/housing["households"]
+housing["bedrooms_per_room"] = housing["total_bedrooms"]/housing["total_rooms"]
+housing["population_per_househlod"] = housing["population"]/housing["households"]
+
+# 상관관계 확인 함수
+corr_matrix = housing.corr(numeric_only=True)
+# print(corr_matrix["median_house_value"].sort_values(ascending=False))
+
+""" Result:
+median_house_value          1.000000
+median_income               0.687151
+room_per_househlod          0.146255
+total_rooms                 0.135140
+housing_median_age          0.114146
+households                  0.064590
+total_bedrooms              0.047781
+population_per_househlod   -0.021991
+population                 -0.026882
+longitude                  -0.047466
+latitude                   -0.142673
+bedrooms_per_room          -0.259952
+"""
+
+
+"""
+# 머신러닝 알고리즘을 위한 데이터 준비
+## 데이터를 수동이 아닌 함수를 만들어 자동화해야 하는 이유
+1. 어떤 데이터셋에 대해서도 데이터 변환을 손쉽게 반복 가능
+2. 향후 프로젝트에서 사용할 수 있는 변환 라이브러리를 점진적으로 구축하게 됨
+3. 실제 시스템에서 알고리즘에 새 데이터를 주입하기 전에 변환시키는데 이 함수를 사용 가능
+4. 여러가지 데이터 변환을 쉽게 시도해볼 수 있고 어떤 조합이 가장 좋은지 확인하는 데 편리함.
+
+median_house_value 열 제거
+
+"""
+
+housing = strat_train_set.drop("median_house_value",axis=1)
+housing_labels = strat_train_set["median_house_value"].copy()
+
+"""
+# 데이터 정제
+특성값이 없을 경우 방법
+1. 해당 구역 제거
+2. 전체 특성 삭제
+3. 임의 값 삽임, 0, 평균, 중간값 등 Ex) dropna, fillna, drop
+"""
+
+from sklearn.impute import SimpleImputer
+#누락된 값은 중간값으로 바꿔주기 위한 인스턴스 생성
+imputer = SimpleImputer(strategy="median")
+# ocen_proximity 열을 제거
+housing_num = housing.drop("ocean_proximity",axis=1)
+# 중앙값을 계산하게 위해 데이터를 채우기 위한 작업 시작
+imputer.fit(housing_num)
+
+# imputer는 각 특성의 중간값을 계산해서 그 결과를 객체의 statistics_ 속성에 저장.
+# print(imputer.statistics_)
+
+X = imputer.transform(housing_num)
+housing_tr = pd.DataFrame(X, columns=housing_num.columns,
+                          index=housing_num.index)
